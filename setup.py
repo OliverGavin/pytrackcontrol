9de56dc4db4f4d75ce42239631621ac8fd9a1ecc
OliverGavin <14161044@studentmail.ul.ie>
2017-10-18 18:15:56 +0100

    Setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pytrackcontrol',
+      version='0.1',
+      description='A Python library for interpreting face and hand tracking and gestures from pytrackvision to control a system (HCI).',
+      url='http://github.com/OliverGavin/pytrackcontrol',
+      author='Oliver Gavin',
+      author_email='oliver@gavin.ie',
+      license='MIT',
+      packages=['pytrackcontrol'],
+      zip_safe=False)
